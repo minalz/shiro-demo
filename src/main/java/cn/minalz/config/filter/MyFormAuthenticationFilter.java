d21ee7a8b097b4988d79a0bdaa455e4cd384b935
minalz <119687281@qq.com>
2020-08-18 07:58:55 +0800

    update filter

--- src/main/java/cn/minalz/config/filter/MyFormAuthenticationFilter.java
+++ src/main/java/cn/minalz/config/filter/MyFormAuthenticationFilter.java
@@ -1,8 +1,13 @@
 package cn.minalz.config.filter;
 
+import cn.minalz.model.Role;
+import cn.minalz.model.User;
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
+import org.apache.shiro.web.util.SavedRequest;
+import org.apache.shiro.web.util.WebUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,6 +19,8 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * 自定义authc权限验证的过滤器
@@ -27,6 +34,35 @@ public class MyFormAuthenticationFilter extends FormAuthenticationFilter {
         // 最好的方式 应该是判断登录后的用户 是否有这个页面的访问权限，如果没有，那么直接跳转到登录页
         // 如果有的话，应该直接往这个就得登录页进行跳转
         // 现在存在的问题是，如果切换了用户，而之前的url，又是现用户无权限访问的，那么就会出现页面显示不友好的情况
+        String successUrl = null;
+        boolean contextRelative = true;
+        SavedRequest savedRequest = WebUtils.getAndClearSavedRequest(request);
+        if (savedRequest != null && savedRequest.getMethod().equalsIgnoreCase("GET")) {
+            successUrl = savedRequest.getRequestUrl();
+            contextRelative = false;
+        }
+
+        User user = (User) subject.getPrincipal();
+        Set<Role> roles = user.getRoles();
+        roles.forEach(x -> {
+            x.getPermissions();
+        });
+
+        /*String successUrl = null;
+        boolean contextRelative = true;
+        SavedRequest savedRequest = WebUtils.getAndClearSavedRequest(request);
+        if (savedRequest != null && savedRequest.getMethod().equalsIgnoreCase("GET")) {
+            successUrl = savedRequest.getRequestUrl();
+            contextRelative = false;
+        }
+
+        if (successUrl == null) {
+            throw new IllegalStateException("Success URL not available via saved request or via the successUrlFallback method parameter. One of these must be non-null for issueSuccessRedirect() to work.");
+        } else {
+            WebUtils.issueRedirect(request, response, successUrl, (Map)null, contextRelative);
+        }*/
+
+
         this.redirectToLogin(request, response);
         return false;
     }
